cbc4279cf1ab66390dce65cdafc0baefc02b67bf
jrodriguezgo2022 <j.rodriguezgo.2022@alumnos.urjc.es>
2024-12-23 15:10:29 +0000

    comments ended

--- esp32/MqttSetr.cpp
+++ esp32/MqttSetr.cpp
@@ -2,6 +2,7 @@
 #include "MqttSetr.hpp"
 
 
+// Class builder
 MqttSetr::MqttSetr(char* server, int port, char* username, char* password)
     : mqtt_client_(&wifi_client_, server, port, username, password),
       publisher_(&mqtt_client_, MQTT_TOPIC),
@@ -9,30 +10,35 @@ MqttSetr::MqttSetr(char* server, int port, char* username, char* password)
     mqtt_client_.subscribe(&subscriber_);
 }
 
+// Reconnect function
 void MqttSetr::reconnect() {
+  // While is not connected
   while (!mqtt_client_.connected()) {
+    // Traze
     Serial.print("Intentando conectar al broker MQTT...");
+    // If it is connected
     if (mqtt_client_.connect()) {
       Serial.println("¡Conectado!");
       // Resuscribirse a los topics necesarios después de reconectar
       mqtt_client_.subscribe(&subscriber_);
-    } else {
-      Serial.println("Error al conectar. Reintentando en 5 segundos...");
-      delay(5000); // Espera antes de intentar reconectar
     }
   }
 }
 
+// Function to publish
 bool MqttSetr::publish(char* buffer) {
+  // Publish and returns true or false
   return publisher_.publish(buffer);
 }
 
+// Function not needed at the end
 void MqttSetr::recive() {
 
 }
 
-
+// Checks if mqtt is connected
 bool MqttSetr::ready() {
+  // Checks if it is connected and decides starting params
   if (mqtt_client_.connected()) {
     mqtt_client_.processPackets(10);
     mqtt_client_.ping();
--- final_mains/esp32.ino
+++ final_mains/esp32.ino
@@ -14,9 +14,6 @@
 
 MqttSetr mqtt_prove = MqttSetr(MQTT_SERVER, MQTT_SERVERPORT, "", "");
 
-// char* ssid = "POCO F3";
-// char* password = "12341234";
-
 // Configuración Wi-Fi
 #define EAP_ANONYMOUS_IDENTITY "20220719anonymous@urjc.es"  // leave as it is
 #define EAP_IDENTITY URJC_EMAIL                             // Use your URJC email
@@ -26,77 +23,117 @@ char* ssid = "eduroam";
 
 long unsigned start_time;
 
+// Creates thread and thread controller for ping
 ThreadController controller = ThreadController();
 Thread pingThread = Thread();
 
+// Char buffer for sending jsons
 char json_buffer[200];
 
+// Setup
 void setup() {
-  // put your setup code here, to run once:
+  // Initializes both serial
   Serial.begin(115200);
   Serial2.begin(38400, SERIAL_8N1, RXD2, TXD2);
   delay(10);
 
-  // Conectar a Wi-Fi
+  // Connects to wifi of eduroam
   Serial.print("Conectando a Wi-Fi...");
   WiFi.begin(ssid, WPA2_AUTH_PEAP, EAP_IDENTITY, EAP_USERNAME, EAP_PASSWORD);
-  // WiFi.begin(ssid, password);
 
+
+  // Goes checking if wifi is already connected with a bit of delay
   while (WiFi.status() != WL_CONNECTED) {
     delay(50);
   }
 
+  // Shows on Serial monitor if its connected
   Serial.println("¡Conectado!");
 
+  // While it not connected to mqqt retries
   while (!mqtt_prove.ready()) {
     mqtt_prove.reconnect();
   }
 
+  // Once is connected starts thread for ping
   pingThread.enabled = true;
   pingThread.setInterval(4000);
+
+  // Creates json for starting lap
   create_json("START_LAP", 0);
+
+  //Publishes json
   mqtt_prove.publish(json_buffer);
+  
+  // Sends message to start on Arduino
   Serial2.println("<Arduino Start>");
+
+  // Starts thread
   pingThread.onRun(pingSender);
+
+  // Adds thread
   controller.add(&pingThread);
+  
+  // Take starting millis
   start_time = millis();
 }
 
+// Loop
 void loop() {
+  // Runs controller
   controller.run();
+
+  // Variables 
   String action;
   int timer;
+
   // Leer datos del Arduino
   if (Serial2.available()) {
     String recibido = Serial2.readStringUntil('\n');  // Leer hasta nueva línea
     recibido.trim();
 
+    // If meesage is correct
     if (validarMensaje(recibido)) {
+      // Takes index of : char
       int index = recibido.indexOf(":");
 
+      // Splits the message into two
       action = recibido.substring(1, index);
       timer = recibido.substring(index + 1, recibido.length() - 1).toInt();
 
+      // Creates json with header and values
       create_json(action, timer);
+
+      // Publishes json
       mqtt_prove.publish(json_buffer);
     }
   }
 }
 
+
+// Function to validate messages
 bool validarMensaje(String mensaje) {
-  // Verificar delimitadores
+  // Checks if starts and ends with delimiters
   return (mensaje.startsWith("<") && mensaje.endsWith(">"));
 }
 
+// Function to send ping
 void pingSender() {
+  // Takes time since start of lap
   unsigned long ping_time = millis() - start_time;
+
+  // Creates message
   create_json("PING", ping_time);
+  
+  // Publishes messages
   mqtt_prove.publish(json_buffer);
 }
 
+// Function to create the json
 void create_json(String function, unsigned long value) {
   StaticJsonDocument<200> jsonDoc;
 
+  // Checks the header and create the json accordingly
   if (function == "PING") {
     jsonDoc["team_name"] = "JorgeScript";
     jsonDoc["id"] = "6";
@@ -127,5 +164,6 @@ void create_json(String function, unsigned long value) {
     jsonDoc["action"] = function;
   }
 
+  // Transforms json into char*
   serializeJson(jsonDoc, json_buffer);
 }
